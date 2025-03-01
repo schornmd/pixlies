eeba6daa66181803e017574af6e71a9c2ab2659c
maurxce <maurice@schorn.md>
2025-03-01 15:19:14 +0100

    add and display season data

---
+++ src/data/seasons.ts
@@ -0,0 +1,31 @@
+interface Season {
+  title: string;
+  end: string;
+}
+
+export default [
+  {
+    title: "Pixlies X",
+    end: "2022-01-29",
+  },
+  {
+    title: "Pixlies V",
+    end: "2021-07-18",
+  },
+  {
+    title: "Pixlies Remastered",
+    end: "2021-03-16",
+  },
+  {
+    title: "Pixlies Reloaded",
+    end: "",
+  },
+  {
+    title: "Pixlies 2.0",
+    end: "2020-02-09",
+  },
+  {
+    title: "Pixlies 1.0",
+    end: "2019-09-10",
+  },
+] as Season[];
